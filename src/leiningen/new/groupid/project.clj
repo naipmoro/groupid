4b30e5e3513cf81b0500bd5890a11669b208d400
naipmoro <naipmoro@gmail.com>
2013-10-11 01:09:31 -0400

    switching to clojure 1.5.1

--- src/leiningen/new/groupid/project.clj
+++ src/leiningen/new/groupid/project.clj
@@ -2,7 +2,4 @@
   :description "description of the project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"} 
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:repositories [["sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-                   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}})
+  :dependencies [[org.clojure/clojure "1.5.1"]])
