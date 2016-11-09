f89e9fc40ae11e6e863d8d02777409f6131af7f0
naipmoro <naipmoro@gmail.com>
2016-11-09 16:42:54 -0500

    Change license to MIT

--- src/leiningen/new/groupid/project.clj
+++ src/leiningen/new/groupid/project.clj
@@ -1,5 +1,5 @@
 (defproject {{long-name}} "0.1.0-SNAPSHOT"
   :description "description of the project"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"} 
+  :license :name "MIT License"
+            :url "http://opensource.org/licenses/MIT" 
   :dependencies [[org.clojure/clojure "1.5.1"]])
