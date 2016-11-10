aa410f2877b2a36decff9efdcbcc0f9053f45da5
naipmoro <naipmoro@gmail.com>
2016-11-09 22:04:39 -0500

    Edit project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject groupid/lein-template "0.2.1"
   :description "A leiningen template for new projects containing a group-id prefix"
+  :url "https://github.com/naipmoro/groupid"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :scm {:name "git"
+        :url "https://github.com/naipmoro/groupid"}
   :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true)
